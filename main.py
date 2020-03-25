009750f0e4b1594f7506d521d1252482a3926862
ajaggarcia10 <45190522+ajaggarcia10@users.noreply.github.com>
2020-03-24 18:00:16 -0700

    Time Microservice Files

---
+++ main.py
@@ -0,0 +1,15 @@
+import time
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_time():
+    return str(time.time())
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 7787))
+    app.run(host='0.0.0.0', port=port)
